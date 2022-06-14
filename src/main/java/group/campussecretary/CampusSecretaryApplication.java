34e04e7e55b14fac9fc84e3e5cafc0eab577cb74
신정아 <jeonga@khu.ac.kr>
2022-06-14 23:30:32 +0900

    campus-secretary project init

---
+++ src/main/java/group/campussecretary/CampusSecretaryApplication.java
@@ -0,0 +1,13 @@
+package group.campussecretary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusSecretaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusSecretaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group/campussecretary/CampusSecretaryApplicationTests.java
@@ -0,0 +1,13 @@
+package group.campussecretary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusSecretaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
