0bfa7d3a154ba25a6266f9419bdc45a6373c2842
신정아 <shinjeonga@twentyoz.kr>
2022-06-15 12:22:19 +0900

    add test code

--- src/test/java/group/campussecretary/CampusSecretaryApplicationTests.java
+++ src/test/java/group/campussecretary/CampusSecretaryApplicationTests.java
@@ -2,12 +2,16 @@ package group.campussecretary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootTest
 class CampusSecretaryApplicationTests {
 
     @Test
     void contextLoads() {
+//        ClassPathResource classPathResource=new ClassPathResource("test.txt");
+//
+//        System.out.println(">>>>>>>>"+classPathResource.getPath());
     }
 
 }
