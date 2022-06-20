bcb104f5189f0b1646ce71422502de19eb01655b
신정아 <shinjeonga@twentyoz.kr>
2022-06-20 11:07:46 +0900

    complete campus to-do-list service

--- src/main/java/group/campussecretary/feature/service/CampusService.java
+++ src/main/java/group/campussecretary/feature/service/CampusService.java
@@ -1,11 +1,19 @@
 package group.campussecretary.feature.service;
 
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,7 +45,9 @@ public class CampusService {
 
     }
 
-    public void activateBot(){
+    public String activateBot(){
+
+        String toDoList="todo";
 
         setWebDriver();
 
@@ -45,32 +55,44 @@ public class CampusService {
             driver.get(url);
             Thread.sleep(2000); // 3. 페이지 로딩 대기 시간
 
+            ClassPathResource resource = new ClassPathResource("login.json");
+            JSONObject json = (JSONObject) new JSONParser().parse(new InputStreamReader(resource.getInputStream(), "UTF-8"));
+            System.out.println(">>>>>>>> "+json.toString());
+
             //id입력
             WebElement element=driver.findElement(By.name("login_user_id"));
-            element.sendKeys("hsjm1999");
+            element.sendKeys(json.get("id").toString());
 
             //비밀번호 입력
             element=driver.findElement(By.name("login_user_password"));
-            element.sendKeys("qlalfqjsgh0613!");
+            element.sendKeys(json.get("pw").toString());
 
             //전송
             element=driver.findElement(By.className("login_btn"));
             element.click();
-
-
             Thread.sleep(1000);
+
+            //내 강의실 들어가기 버튼 클릭
             // //*[@id="visual"]/div/div[2]/div[2]/div[1]/a
             element= driver.findElement(By.xpath("//*[@id=\"visual\"]/div/div[2]/div[2]/div[1]/a"));
             element.click();
-
             Thread.sleep(2000);
+
+            //할 일 목록
+            element= driver.findElement(By.xpath("//*[@id=\"right-side\"]/div[2]/div/span"));
+            //System.out.println(">>>>>>>>>>>>>getText: "+element.getText());
+            Thread.sleep(2000);
+
+            toDoList = element.getText();
 
         }catch (Exception e) {
             e.printStackTrace();
         }finally{
             //브라우저 종료
             driver.close();
+
         }
 
+        return toDoList;
     }
 }
