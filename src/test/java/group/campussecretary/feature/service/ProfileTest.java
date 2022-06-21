2196afe4bb55dfa3e35a05bcd4cbb8554b19f4f4
신정아 <shinjeonga@twentyoz.kr>
2022-06-21 16:49:16 +0900

    add string list parsing test

--- src/test/java/group/campussecretary/feature/service/ProfileTest.java
+++ src/test/java/group/campussecretary/feature/service/ProfileTest.java
@@ -1,6 +1,8 @@
 package group.campussecretary.feature.service;
 
 import group.campussecretary.feature.model.Profile;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -38,6 +40,33 @@ public class ProfileTest {
     service.modify(UUID.fromString("9f1bf68f-3e3a-433d-ab65-e5613cbbc0ba"),profile2);
 
     System.out.println(profile.toString());
+
+  }
+
+  @Test
+  public void stringListParsing(){
+    List<String> list = new ArrayList<>();
+    list.add("코로나");
+    list.add("학교");
+
+    String str = list.toString();
+    System.out.println(str); //[코로나, 학교]
+
+    String match = "[^\uAC00-\uD7A30-9a-zA-Z]";
+
+    str = str.replaceAll(match, " ");
+
+    System.out.println(str); // 코로나  학교
+
+    String[] splitStr = str.split("\\s+");
+
+    for(int i=0;i<splitStr.length;i++){
+      System.out.println(splitStr[i]);
+    }
+    /**
+     * 코로나
+     * 학교
+     */
 
   }
 }
