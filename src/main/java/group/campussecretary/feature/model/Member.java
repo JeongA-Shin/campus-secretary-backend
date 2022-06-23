ebb99f2be03f4c443fca785b12930611a0eccb91
신정아 <shinjeonga@twentyoz.kr>
2022-06-23 15:04:42 +0900

    modify Member entity to implement UserDetails

--- src/main/java/group/campussecretary/feature/model/Member.java
+++ src/main/java/group/campussecretary/feature/model/Member.java
@@ -1,5 +1,11 @@
 package group.campussecretary.feature.model;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,6 +18,9 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
@@ -21,11 +30,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @NoArgsConstructor
 @ToString
 @EqualsAndHashCode
-public class Member {
+public class Member implements UserDetails {
+  //UserDetails : spring security로 관리하고 싶은 (회원) 객체는 UserDetils 인터페이스르 반드시 구현해야 한다고 보면 됨!
+  // UserDetails: SUserDetailspring Security에서 사용자 객체의 정보를 불러오기 위해서 필수적으로 구현해야 하는 인터페이스
+  //즉 일단은 spring security로 관리할 객체면 UserDetails를 구현해야 한다고 보면 됨
 
-  /**
-   * 회원객체
-   */
+  private static final String ROLE_PREFIX = "ROLE_";
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -47,19 +57,56 @@ public class Member {
   /**
    * 패스워드
    */
-  @Column(name = "password",unique = true)
+  @Column(name = "password")
   private String password;
 
   /*
   권한
    */
-  @Column(name = "role",unique = true)
-  private String role;
+  //여러 개인 경우 그냥 쉼표(,)로 구분함
+  //여기는 그냥 "내"가 관리하는 권한 (ADMIN, USER)
+  //spring security는 아래의 getAuthorities을 통해 얻어지는 authorities들을 관리함 - ROLE_ADMIN , ROLE_USER
+  @Column(name = "roles")
+  private String roles;
 
 
   public void encodePassword(PasswordEncoder passwordEncoder){
     //패스워드 암호화해서 db에 저장하기
     this.password = passwordEncoder.encode(password);
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+
+    Set<GrantedAuthority> authorities= new HashSet<>();
+    for (String role : roles.split(",")) {
+      authorities.add(new SimpleGrantedAuthority(ROLE_PREFIX + role));
+    }
+    return authorities;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    // 만료되었는지 확인하는 로직
+    return true; // true -> 만료되지 않았음
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    // 계정 잠금되었는지 확인하는 로직
+    return true; // true -> 잠금되지 않았음
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    // 패스워드가..? 만료되었는지 확인하는 로직
+    return true; // true -> 만료되지 않았음
+  }
+
+  @Override
+  public boolean isEnabled() {
+    // 계정이 사용 가능한지 확인하는 로직
+    return true; // true -> 사용 가능
   }
 
 }
